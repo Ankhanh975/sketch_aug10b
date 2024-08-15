9a3d3a3e279e3ec84adc6192a414b6465d331b74
Your Name <ankhanh1566@gmail.com>
2024-08-15 15:09:32 +0700

    Refactor walking algorithm code for improved readability and maintainability

--- sketch_aug10b.ino
+++ sketch_aug10b.ino
@@ -7,30 +7,23 @@ int sign(float number)
 class ServoMaster
 {
 private:
-    int servoPins[4];  // Pins connected to the servos
-    int numServos = 4; // Number of servos
-    Servo servos[4];   // Create an array of Servo objects
+    const int numServos = 4; // Number of servos
 
-    int offsetServo0 = -5;
-    int offsetServo1 = -7;
-    int offsetServo2 = 20;
-    int offsetServo3 = +0;
+    int servoPins[] = {12, 9, 11, 10}; // Pins connected to the servos
 
-    int angle1 = 90;
-    int angle2 = 90;
-    int angle3 = 90;
-    int angle4 = 90;
+    int offsetServo[] = {-5, -7, 20, 0};
 
-    int set_angle1 = 90;
-    int set_angle2 = 90;
-    int set_angle3 = 90;
-    int set_angle4 = 90;
+    int angle[] = {90, 90, 90, 90};
+
+    int set_angle[] = {90, 90, 90, 90};
+
+    Servo servos[numServos]; // Create an array of Servo objects
 
     void attach()
     {
         for (int i = 0; i < this->numServos; i++)
         {
-            servos[i].attach(servoPins[i]);
+            this->servos[i].attach(servoPins[i]);
             int initialPosition = 90;        // Default initial position
             this->write(i, initialPosition); // Set each servo to its corrected initial position
         }
@@ -42,27 +35,12 @@ private:
             initialPosition = 180 - initialPosition;
         }
         int _angle;
-        if (servoNum == 0)
-        {
-            initialPosition += offsetServo0;
-        }
-        else if (servoNum == 1)
-        {
-            initialPosition += offsetServo1;
-        }
-        else if (servoNum == 2)
-        {
-            initialPosition += offsetServo2;
-        }
-        else if (servoNum == 3)
-        {
-            initialPosition += offsetServo3;
-        }
+        initialPosition += offsetServo[servoNum];
         _angle = constrain(initialPosition, 0, 180); // Ensure position is within bounds
         float final_pos;
-         if (servoNum == 0)
+        if (servoNum == 0)
         {
-            final_pos = (_angle - 90) * 1.67 + 90;
+            final_pos = (_angle - 90) * 1.65 + 90;
         }
         else if (servoNum == 1)
         {
@@ -74,7 +52,7 @@ private:
         }
         else if (servoNum == 3)
         {
-            final_pos = (_angle - 90) * 1.67 + 90;
+            final_pos = (_angle - 90) * 1.65 + 90;
         }
         this->servos[servoNum].write(final_pos);
     }
@@ -82,66 +60,24 @@ private:
 public:
     void init()
     {
-        this->servoPins[0] = 12;
-        this->servoPins[1] = 9;
-        this->servoPins[2] = 11;
-        this->servoPins[3] = 10;
-
         this->attach();
     }
     void update()
     {
-        for (int i = 0; i < 2; i++)
+
+        for (int ii = 0; i < 4; i++)
         {
-            if (set_angle1 != angle1)
+            for (int i = 0; i < 2; i++)
             {
-
-
-                angle1 += sign(set_angle1 - angle1);
-                write(0, angle1);
-            }
-            else
-            {
-                break;
-            }
-        }
-        for (int i = 0; i < 2; i++)
-        {
-            if (set_angle2 != angle2)
-            {
-
-                angle2 += sign(set_angle2 - angle2);
-                write(1, angle2);
-            }
-            else
-            {
-                break;
-            }
-        }
-        for (int i = 0; i < 2; i++)
-        {
-            if (set_angle3 != angle3)
-            {
-
-                angle3 += sign(set_angle3 - angle3);
-                write(2, angle3);
-            }
-            else
-            {
-                break;
-            }
-        }
-        for (int i = 0; i < 2; i++)
-        {
-            if (set_angle4 != angle4)
-            {
-
-                angle4 += sign(set_angle4 - angle4);
-                write(3, angle4);
-            }
-            else
-            {
-                break;
+                if (set_angle[ii] != angle[ii])
+                {
+                    angle[ii] += sign(set_angle[ii] - angle[ii]);
+                    write(ii, angle[ii]);
+                }
+                else
+                {
+                    break;
+                }
             }
         }
     }
@@ -149,125 +85,86 @@ public:
     {
 
         int _angle = constrain(angle, 0, 180); // Ensure position is within bounds
-        // Apply offsets for specific servos
-        if (servoNum == 0)
-        {
-            set_angle1 = _angle;
-        }
-        else if (servoNum == 1)
-        {
-            set_angle2 = _angle;
-        }
-        else if (servoNum == 2)
-        {
-            set_angle3 = _angle;
-        }
-        else if (servoNum == 3)
-        {
-            set_angle4 = _angle;
-        }
+        this->set_angle[servoNum] = _angle;
     }
 
     void setAngleImidiately(int servoNum, int angle)
     {
         this->write(servoNum, angle);
-        if (servoNum == 0)
-        {
-            angle1 = angle;
-            set_angle1 = angle;
-        }
-        else if (servoNum == 1)
-        {
-            angle2 = angle;
-            set_angle2 = angle;
-        }
-        else if (servoNum == 2)
-        {
-            angle3 = angle;
-            set_angle3 = angle;
-        }
-        else if (servoNum == 3)
-        {
-            angle4 = angle;
-            set_angle4 = angle;
-        }
+        this->angle[servoNum] = angle;
+        this->set_angle[servoNum] = angle;
     }
     int get_angle(int num)
     {
-        if (num == 0)
-        {
-            return angle1;
-        }
-        else if (num == 1)
-        {
-            return angle2;
-        }
-        else if (num == 2)
-        {
-            return angle3;
-        }
-        else if (num == 3)
-        {
-            return angle4;
-        }
+        return angle[num];
     }
 };
 
 ServoMaster servoMaster;
-long frameCount = 0;   
+long frameCount = 0;
 void setup()
 {
-  servoMaster.init();
-  servoMaster.setAngle(0, 120);
-      servoMaster.setAngle(1, 60);
-      servoMaster.setAngle(2, 60);
-      servoMaster.setAngle(3, 120);
-      for(int i; i<10;i++){
+    servoMaster.init();
+    servoMaster.setAngle(0, 120);
+    servoMaster.setAngle(1, 60);
+    servoMaster.setAngle(2, 60);
+    servoMaster.setAngle(3, 120);
+    for (int i; i < 10; i++)
+    {
         delay(5);
         servoMaster.update();
-      }
-
+    }
 }
 void loop()
 {
     delay(6);
     frameCount++;
     servoMaster.update();
-    int state = (frameCount%170);
-    if(state==0){
-      servoMaster.setAngle(0, 100);
-      servoMaster.setAngle(3, 100);
-    } 
-    if(state==20){
-      servoMaster.setAngle(2, 125);
+    int state = (frameCount % 170);
+    if (state == 0)
+    {
+        servoMaster.setAngle(0, 100);
+        servoMaster.setAngle(3, 100);
     }
-    if(state==30){
-      servoMaster.setAngle(0, 80);
-      servoMaster.setAngle(3, 80);
-    } 
-    if(state==50){
-      servoMaster.setAngle(1, 125);
+    if (state == 20)
+    {
+        servoMaster.setAngle(2, 125);
     }
-    if(state==60){
-      servoMaster.setAngle(0, 55);
-      servoMaster.setAngle(3, 55);
-    } 
-     if(state==80){
-      servoMaster.setAngle(1, 100);
-      servoMaster.setAngle(2, 100);
-    } 
-    if(state==100){
-      servoMaster.setAngle(3, 125);
+    if (state == 30)
+    {
+        servoMaster.setAngle(0, 80);
+        servoMaster.setAngle(3, 80);
     }
-    if(state==110){
-      servoMaster.setAngle(1, 80);
-      servoMaster.setAngle(2, 80);
-    } 
-    if(state==130){
-      servoMaster.setAngle(0, 120);
+    if (state == 50)
+    {
+        servoMaster.setAngle(1, 125);
     }
-    if(state==150){
-      servoMaster.setAngle(1, 55);
-      servoMaster.setAngle(2, 55);
-    }} 
-
+    if (state == 60)
+    {
+        servoMaster.setAngle(0, 55);
+        servoMaster.setAngle(3, 55);
+    }
+    if (state == 80)
+    {
+        servoMaster.setAngle(1, 100);
+        servoMaster.setAngle(2, 100);
+    }
+    if (state == 100)
+    {
+        servoMaster.setAngle(3, 125);
+    }
+    if (state == 110)
+    {
+        servoMaster.setAngle(1, 80);
+        servoMaster.setAngle(2, 80);
+    }
+    if (state == 130)
+    {
+        servoMaster.setAngle(0, 120);
+    }
+    if (state == 150)
+    {
+        servoMaster.setAngle(1, 55);
+        servoMaster.setAngle(2, 55);
+    }
+}
